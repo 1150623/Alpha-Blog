40df2003b64b86b0553ae03a7ad44a20bfd714ca
Guilherme Ferreira <1150623@isep.ipp.pt>
2018-05-14 23:46:22 +0100

    Added Admin atribute to users [default=false]

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -84,7 +84,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_author
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can only edit or delete your articles."
         redirect_to root_path
       end
