9f65e4e5817e84332f756173dbe6365988b612d7
Guilherme Ferreira <1150623@isep.ipp.pt>
2018-05-14 14:16:35 +0100

    some changes + complete form for signup users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "#{@user.username}, welcome to Alpha Blog!"
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permite(:username, :email, :password)
+  end
+
 end
