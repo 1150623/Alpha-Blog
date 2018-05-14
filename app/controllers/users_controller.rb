46543badd3f595c43e7439eff279fb660db0e96f
Guilherme Ferreira <1150623@isep.ipp.pt>
2018-05-14 23:58:07 +0100

    Added admin right to delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_author, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_author, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -35,6 +36,12 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user.destroy
+    flash[:notice] = "User and respective articles were all deleted!"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -45,8 +52,15 @@ class UsersController < ApplicationController
   end
 
   def require_author
-    if current_user != @user
+    if current_user != @user and !current_user.admin?)
       flash[:danger] = "You can only edit or delete your information."
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? && !current_user.admin?
+      flash[:danger] = "Only admin users can perform this action!"
       redirect_to root_path
     end
   end
