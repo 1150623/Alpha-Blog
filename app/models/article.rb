01deb7cb7f57fc71b6e58c722b5c2863f81cf762
Guilherme Ferreira <1150623@isep.ipp.pt>
2018-05-15 22:58:17 +0100

    finished funcionalities of categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :aticle_categories
+  has_many :article_categories
   has_many :categories, through: :article_categories
   # presence -> Must not be nil
   # length -> must be of length x
