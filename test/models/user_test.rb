f4df481e26c1acc3759ba6fcf12d4b9ec687192b
Guilherme Ferreira <1150623@isep.ipp.pt>
2018-05-16 10:51:47 +0100

    added test subjects of user

---
+++ test/models/user_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+  end
+
+  test "User can't have empty username" do
+
+  end
+
+  test "User can't have empty email" do
+
+  end
+  
+  test "User can't have invalid email - regex" do
+
+  end
+
+  test "Username must have specific length" do
+
+  end
+
+  test "Username must have password" do
+
+  end
+
+  test "Username must be unique" do
+
+  end
+
+  test "Email must be unique" do
+
+  end
+
+end
